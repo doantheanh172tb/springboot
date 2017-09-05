4cee5cf9d9b023cf9fe3e02d215c22520311c9e4
vTigerCRM_91 <contact.dfr.hta@gmail.com>
2017-09-05 23:26:38 +0700

    01. ApplicationContext

--- springmvc/src/main/java/theanh/springmvc/SpringmvcApplication.java
+++ springmvc/src/main/java/theanh/springmvc/SpringmvcApplication.java
@@ -2,11 +2,20 @@ package theanh.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringmvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringmvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SpringmvcApplication.class, args);
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+
+        for (String name : beanNames) {
+            System.out.println(name);
+        }
+    }
 }
