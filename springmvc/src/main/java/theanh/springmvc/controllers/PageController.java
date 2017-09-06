112694ddf053f7a2fd674ae468128a1be2f4b8f0
vTigerCRM_91 <contact.dfr.hta@gmail.com>
2017-09-06 16:27:39 +0700

    01.Application properties

--- springmvc/src/main/java/theanh/springmvc/controllers/PageController.java
+++ springmvc/src/main/java/theanh/springmvc/controllers/PageController.java
@@ -1,6 +1,7 @@
 package theanh.springmvc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import theanh.springmvc.services.NotificationService;
@@ -9,11 +10,19 @@ import theanh.springmvc.services.NotificationService;
 public class PageController {
 
     //notificationService
-    @Autowired
     private NotificationService notificationService;
+
+    @Autowired
+    public PageController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @Value("${my.secret}")
+    public String mySecret;
 
     @RequestMapping("/")
     public String home() {
+        System.out.println("PageController::mySecret------" + mySecret);
         return notificationService.toString();
     }
 
